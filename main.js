f2b4e933d8062f34091f9022aeea05b619f629a3
lucky07070707 <165228213+lucky07070707@users.noreply.github.com>
2024-03-27 22:26:26 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+alert ('parça')
+
+let nomeUsuario = '';
+
+let texto = document.querySelector('span');
+
+while(nomeUsuario == ''){
+nomeUsuario = prompt ('Qual seu nome?');
+}
+
+if(nomeUsuario == null){
+texto.textContent = 'Usuário'
+} else{texto.textContent = nomeUsuario;
+      
+}
